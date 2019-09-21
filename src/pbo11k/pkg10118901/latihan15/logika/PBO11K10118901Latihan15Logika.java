74bdafecba72bda2d015b9d545231da3b841e9c2
Gonior <43252085+Gonior@users.noreply.github.com>
2019-09-21 23:40:12 +0700

    Dibuat : Sabtu, 21 September 2019

---
+++ src/pbo11k/pkg10118901/latihan15/logika/PBO11K10118901Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA         : Dedi Cahya 
+ * KELAS        : IF-11-K 
+ * NIM          : 10118901 
+ * DESKRIPSI    : Mengenal operasi logika dalam java
+ */
+public class PBO11K10118901Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true ;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
